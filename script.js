34bb7a498ebec15c62fa2bca61456c7f52204544
Dimario <Dimario-one@yandex.ru>
2022-05-03 14:29:05 +0600

    create

--- script.js
+++ script.js
@@ -1,103 +1,194 @@
-let button=document.getElementById("button");
+let button = document.getElementById("button");
 let input = document.getElementById("input-point");
 let inputTime = document.getElementById("input-time");
-let addButton=document.getElementById("addpoint");
+let addButton = document.getElementById("addpoint");
+let plan = document.getElementById("input-plan");
 
 // Фактический график
 var trace1 = {
-    x: [ '2022-04-15 00:00:00', '2022-04-15 23:59:59'],
-    y: [1, 5, 3, 10, 5],
-    mode: "lines+markers",
-    type: "scatter",
-    line: {color: '#ff0000'}
+  x: [
+    1649959200, 1649962800, 1649966400, 1649970000,
+    //  1649973600, 1649977200,
+    // 1649980800, 1649984400, 1649988000, 1649991600, 1649995200, 1649995200,
+    // 1650002400, 1650006000, 1650009600, 1650013200, 1650016800, 1650020400,
+    // 1650024000, 1650027600, 1650031200, 1650034800, 1650038400, 1650042000,
+    1650045600,
+  ],
+  y: [1, 5, 3, 5, 10],
+  mode: "lines",
+  type: "scatter",
+  line: { color: "#ff0000" },
 };
 // Добыто за час
 var trace2 = {
-    x: ['2022-04-15 00:00:00', '2022-04-15 23:59:59'],
-    y: [4, 8],
-    mode: "lines+markers",
-    type: "bar",
-
+  x: [
+    1649959200,
+    // 1649962800, 1649966400, 1649970000, 1649973600, 1649977200,
+    // 1649980800, 1649984400, 1649988000, 1649991600, 1649995200, 1649995200,
+    // 1650002400, 1650006000, 1650009600, 1650013200, 1650016800, 1650020400,
+    // 1650024000, 1650027600, 1650031200, 1650034800, 1650038400, 1650042000,
+    1650045600,
+  ],
+  y: [4, 8],
+  mode: "lines+markers",
+  type: "bar",
 };
-// Прогноз
+// план
 var trace3 = {
-    x: [ '2022-04-15 00:00:00', '2022-04-16 00:00:00'],
-    y: [15,15],
-    mode: "lines+markers",
-    type: "scatter",
-    line: {color: '#000'}
+  x: [
+    1649959200,
+    //  1649962800, 1649966400, 1649970000, 1649973600, 1649977200,
+    // 1649980800, 1649984400, 1649988000, 1649991600, 1649995200, 1649995200,
+    // 1650002400, 1650006000, 1650009600, 1650013200, 1650016800, 1650020400,
+    // 1650024000, 1650027600, 1650031200, 1650034800, 1650038400, 1650042000,
+    1650045600,
+  ],
+  y: [],
+  mode: "lines+markers",
+  type: "scatter",
+  line: { color: "#000" },
+};
+// прогноз
+var trace4 = {
+  x: [
+    1649959200,
+    //  1649962800, 1649966400, 1649970000, 1649973600, 1649977200,
+    // 1649980800, 1649984400, 1649988000, 1649991600, 1649995200, 1649995200,
+    // 1650002400, 1650006000, 1650009600, 1650013200, 1650016800, 1650020400,
+    // 1650024000, 1650027600, 1650031200, 1650034800, 1650038400, 1650042000,
+    1650045600,
+  ],
+  y: [],
+  mode: "markers",
+  type: "scatter",
+  line: { color: "#0f0" },
 };
 
-//  Функция добавления точки на графике
-function setValue(value, date) {
-    let newData = Object.entries(trace1);
-    let arr = [];
-    let elementX;
-    let elementY;
-    // Создаем массив
-    arr.push(newData[0]);
-    arr.push(newData[1]);
+var data = [trace1, trace2, trace3, trace4];
+// Прогноз
+function forecast() {
+  trace4.x = trace1.x;
+  trace4.y = trace1.y;
+  let raznx = trace1.x[trace1.x.length - 1] - trace1.x[trace1.x.length - 2];
+  let razny = trace1.y[trace1.y.length - 1] - trace1.y[trace1.y.length - 2];
+  for(i=0;i<4;i++){
+    let nextX = raznx + trace1.x[trace1.x.length - 1];
+    let nextY = razny + trace1.y[trace1.y.length - 1];
+    trace4.x.push(nextX);
+    sortArray(trace4.x);
+    trace4.y.push(nextY);
+  }
+  // sortArray( trace4.y);
+  Plotly.update("plotDiv", data, layout);
+}
 
-    for (i = 0; i < arr.length; i++) {
-        let elem = arr[i];
-        for (a = 0; a < elem.length; a++) {
-            // Ось икс
-            if (elem[a] === "x") {
-                elementX = elem[a + 1];
-                for (b = 0; b < elementX.length; b++) {
-                    if (date < elementX[b]) {
-                        // Если дата меньше элемента массива
-                        elementX.splice(b, 0, date);
-                        console.log(elementX);
-                    } else {
-                        // если дата равна элементу массива
-                        if (date === elementX[b]) {
-                            elementX.splice(b, 1, date);
-                        } else {
-                            // если дата больше всех элементов массива
-                            elementX.push(date);
-                        }
-                    }
-                }
-            } else {
-                // Ось y
-                if (elem[a] === "y") {
-                    elementY = elem[a + 1];
-                    for (b = 0; b < elementY.length; b++) {
-                        if (value < elementY[b]) {
-                            // Если value меньше элемента массива
-                            elementY.splice(b, 0, value);
-                            console.log(elementY);
-                        } else {
-                            // если value равна элементу массива
-                            if (value === elementY[b]) {
-                                elementY.splice(b, 1, value);
-                            } else {
-                                // если value больше всех элементов массива
-                                elementY.push(value);
-                            }
-                        }
-                    }
-                }
-            }
-        }
+//Функция перевода времени из юникс формата
+function convertFromUnixTime(timestamp, utc = false) {
+  if (!timestamp) return "";
+  let time = "";
+  if (!utc) {
+    let dt = new Date(timestamp * 1000);
+
+    year = dt.getFullYear();
+    month = ("0" + (dt.getMonth() + 1)).slice(-2);
+    day = ("0" + dt.getDate()).slice(-2);
+    hours = ("0" + dt.getHours()).slice(-2);
+    minutes = ("0" + dt.getMinutes()).slice(-2);
+    seconds = ("0" + dt.getSeconds()).slice(-2);
+    time = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  } else {
+    let dt = new Date(timestamp * 1000);
+
+    year = dt.getUTCFullYear();
+    month = ("0" + (dt.getUTCMonth() + 1)).slice(-2);
+    day = ("0" + dt.getUTCDate()).slice(-2);
+    hours = ("0" + dt.getUTCHours()).slice(-2);
+    minutes = ("0" + dt.getUTCMinutes()).slice(-2);
+    seconds = ("0" + dt.getUTCSeconds()).slice(-2);
+
+    time = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  }
+
+  return time;
+}
+// Проверка отсортирован массив или нет
+function isSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < array[i - 1]) {
+      return false;
     }
-    trace1.x=elementX;
-    trace1.y=elementY;
-    Plotly.update("plotDiv", data, layout, { editable: true });
+  }
+  return true;
+}
+
+// Сортировка массива
+function sortArray(array) {
+  const newArray = array.sort((a, b) => a - b);
+  return newArray;
+}
+
+// Добавление точки
+function addPoint(value, time) {
+  let newData = Object.entries(trace1);
+  // Ось Х время
+  trace1.x = [];
+  let arrX = [];
+  let aaa = newData[0];
+  arrX = aaa[1];
+  arrX.push(parseInt(time));
+  trace1.x = sortArray(arrX);
+  // Ось Y значение
+  trace1.y = [];
+  let arrY = [];
+  let ddd = newData[1];
+  arrY = ddd[1];
+  arrY.push(parseInt(value));
+  trace1.y = sortArray(arrY);
+  rendergraph(data, 2);
+}
+
+// Функция перевода из unixtime
+
+// Функция отрисовки графика после добавлени точки
+function rendergraph(data, value = 1) {
+  // for (i = 0; i < data.length; i++) {
+  //   //
+  // //  data[i].x= data[i].x.map((item) => convertFromUnixTime(item));
+  //   //
+  // }
+  if (value === 1) {
+    Plotly.newPlot("plotDiv", data, layout);
+    // forecast();
+  } else {
+    Plotly.update("plotDiv", data, layout);
+    forecast();
+  }
 }
 
 // Функция устанавливающая план добычи
-function setPlan(value){
-for(i=0;i< trace3.y.length;i++){
-    trace3.y=value;
-}
- 
-    Plotly.update("plotDiv", data, layout, { editable: true });
+function setPlan(value) {
+  let num = parseInt(value);
+  trace3.y.push(num);
+  trace3.y.push(num);
+  // for (i = 0; i < trace3.y.length; i++) {
+  //   trace3.y = value;
+  // }
+  rendergraph(data, 2);
 }
 
-var data = [trace1, trace2,trace3];
-var layout = { title: "Click Here<br>to Edit Chart Title" };
-Plotly.newPlot("plotDiv", data, layout, { editable: true });
-addButton.addEventListener("click",()=>{setValue(input.value, inputTime.value)})
-button.addEventListener("click",()=>{setValue(15, 25)});
+var layout = {
+  title: "Click Here<br>to Edit Chart Title",
+  xaxis: {
+    type: "date",
+    dtick: 86400000,
+  },
+};
+rendergraph(data);
+
+addButton.addEventListener("click", () => {
+  addPoint(input.value, inputTime.value);
+});
+
+button.addEventListener("click", () => {
+  setPlan(plan.value);
+});
